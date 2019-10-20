6ef523a7e56d9747252c445a0803d96eded8a42f
naveenkashyapdv <d.naveenkashyap@gmail.com>
2019-10-19 23:27:58 -0700

    Update disablepagefile.ps1

--- disablepagefile.ps1
+++ disablepagefile.ps1
@@ -1,7 +1,6 @@
-#Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-#choco install powershell --y
-#choco install powershell.portable --y
-#sleep 10s
-#$PageFile = Get-WmiObject -Query "Select * From Win32_PageFileSetting Where Name='D:\\pagefile.sys'" -EnableAllPrivileges
-#$PageFile.Delete()
-Restart-Computer -Wait
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install powershell --y
+choco install powershell.portable --y
+sleep 10s
+$PageFile = Get-WmiObject -Query "Select * From Win32_PageFileSetting Where Name='D:\\pagefile.sys'" -EnableAllPrivileges
+$PageFile.Delete()
